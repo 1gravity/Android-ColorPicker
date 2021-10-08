1a1b4a761ad206ee52285b7006858765e3924903
Emanuel Moecklin <emanuel.moecklin@gmail.com>
2021-10-08 17:14:49 -0400

    commit to test Github Actions

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -117,7 +117,7 @@ afterEvaluate {
             val props = project.properties
 
             props.forEach {
-                println("#{it.key} -> ${it.value}")
+                println("${it.key} -> ${it.value}")
             }
             // 1. configure repositories
             repositories {
